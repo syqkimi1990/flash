1022e4a956cf759c206f437501754c10d5f5f3e4
Yuqing Shen <yuqing.shen@ca.abb.com>
2016-10-20 15:34:04 -0700

    Increase battery life and fix multiple click problem

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,29 @@
+/*
+* Constant define
+*/
+var BATTERY_LIFE = 400 //4000 milliseconds
+
+/*
+**  Global variable define
+*/
 var resultDiv;
 var timer;
-var counter = 100;
+var counter = BATTERY_LIFE;
 var flashLightButton = document.getElementById("flashLight");
 var rechargeButton = document.getElementById("recharge");
 var resetButton = document.getElementById("reset");
 
+/*
+ ** Add event listeners
+ */
 document.addEventListener("deviceready", init, false);
 document.addEventListener("backbutton", function() {
 	// pass exitApp as callbacks to the switchOff method
 	window.plugins.flashlight.switchOff(exitApp, exitApp);
 }, false);
 document.addEventListener("resume", onResume, false);
+
+
 
 function init() {
 	document.querySelector("#recharge").addEventListener("touchend", startScan, false);
@@ -20,11 +33,13 @@ function init() {
 }
 
 function startScan() {
+	//disable recharge button when it is pressed
 	rechargeButton.classList.add("disabled");
 	setTimeout(function(){
 		rechargeButton.classList.remove("disabled");
 	}, 500)
 
+	//If recharge button is pressed when the flash light is on, turn off first
 	if (window.plugins.flashlight.isSwitchedOn()) {
 		window.plugins.flashlight.switchOff(scanQrCode, function() {window.alert("Flashlight switch on fails");});
 		window.clearInterval(timer);
@@ -32,26 +47,37 @@ function startScan() {
 	} else {
 		scanQrCode();
 	}
-
 }
 
 function flashLight() {
 
+	//disable the flashlight button to prevent faster click
+	document.querySelector("#flashLight").removeEventListener("touchend", flashLight, false);
+	flashLightButton.classList.add("disabled");
+	setTimeout(function(){
+    	document.querySelector("#flashLight").addEventListener("touchend", flashLight, false);
+		flashLightButton.classList.remove("disabled");
+    }, 700)
+
 	window.plugins.flashlight.available(function(isAvailable) {
 		if (isAvailable) {
 
+			//Switch on the flashlight
 			if (window.plugins.flashlight.isSwitchedOn() == false && counter > 0 && flashLightButton.innerHTML == "Open Flashlight") {
-				flashLightButton.innerHTML = "Close Flashlight";
-				// switch on
+
+				// switch on flash light
 				window.plugins.flashlight.switchOn(
 					function () {
-					}, // optional success callback
+					}, // success callback
 					function () {
 						window.alert("Flashlight switch on fails");
-					}, // optional error callback
-					{intensity: 0.3} // optional as well
+						window.plugins.flashlight.switchOff();
+					}, // error callback
+					{intensity: 0.3}
 					);
 
+				flashLightButton.innerHTML = "Close Flashlight";
+				// Start timer when the flash light is on
 				timer = window.setInterval(function() {
 					counter--;
 					if(counter < 0) {
@@ -59,10 +85,12 @@ function flashLight() {
 						window.clearInterval(timer);
 						counter = 0;
 						flashLightButton.innerHTML = "No More Battery";
+						flashLightButton.classList.add("disabled");
 					} else {
 						resultDiv.innerHTML = "Battery Time Left (s):" + (counter/10).toFixed(1).toString();
 					}
 				}, 100);
+
 
 			} else if (flashLightButton.innerHTML=="Close Flashlight") {
 			    if (timer) {
@@ -70,31 +98,18 @@ function flashLight() {
                     timer = null;
                 }
 
-				if(counter <= 0){
-					window.alert('No time left (10s used up)');
-				} else {
-					//window.alert('left time' + counter/10);
-				}
+				flashLightButton.innerHTML = "Open Flashlight";
 
 				window.plugins.flashlight.switchOff(
 					function () {
-					flashLightButton.innerHTML = "Open Flashlight";
 					}, // optional success callback
 					function () {
 						window.alert("Flashlight switch off fails");
 					}
 				);
 			} else {
-				//do nothing
+				//do nothing as in this condition the battery may run out
 			}
-
-			document.querySelector("#flashLight").removeEventListener("touchend", flashLight, false);
-			flashLightButton.classList.add("disabled");
-			setTimeout(function(){
-				document.querySelector("#flashLight").addEventListener("touchend", flashLight, false);
-				flashLightButton.classList.remove("disabled");
-			}, 1000)
-
 
 		} else {
 			window.alert("Flashlight not available on this device");
@@ -104,11 +119,16 @@ function flashLight() {
 }
 
 function reset() {
+
+	//disable the flashlight button for 0.5s when the reset is pressed to prevent quick click
 	resetButton.classList.add("disabled");
+	document.querySelector("#flashLight").removeEventListener("touchend", flashLight, false);
 	setTimeout(function(){
+		document.querySelector("#flashLight").addEventListener("touchend", flashLight, false);
 		resetButton.classList.remove("disabled");
 	}, 500)
 
+	//if the reset is pressed when the light is on, turn off it first
 	if (window.plugins.flashlight.isSwitchedOn()) {
 		window.plugins.flashlight.switchOff();
 		if (timer) {
@@ -116,9 +136,11 @@ function reset() {
 		}
 	}
 
+	//Reset the program variables
 	timer = null;
 	flashLightButton.innerHTML = "Open Flashlight";
-	counter = 100;
+	flashLightButton.classList.remove("disabled");
+	counter = BATTERY_LIFE;
 	resultDiv.innerHTML = "Battery Time Left (s):" + (counter/10).toFixed(1).toString();
 }
 
@@ -130,7 +152,7 @@ function scanQrCode() {
 				{
 					if(result.format == "QR_CODE")
 					{
-						counter = 100;
+						counter = BATTERY_LIFE;
 						resultDiv.innerHTML = "Battery Time Left (s):" + (counter/10).toFixed(1).toString();
 						window.alert("You have another 10s to use");
 						flashLightButton.innerHTML = "Open Flashlight";
